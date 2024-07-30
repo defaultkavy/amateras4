2a6a963b58d70cefab2b7125eb907d7d1aa3c5f5
defaultkavy <defaultkavy@gmail.com>
2024-07-31 00:54:53 +0800

    fix: twitter link validation

--- src/command/x.ts
+++ src/command/x.ts
@@ -6,7 +6,7 @@ export const cmd_x = new Command('x', 'X（Twitter）链接转换指令', true)
 .string('url', 'X（Twitter）贴文链接', {required: true})
 .execute(async (i, options) => {
     options.url = options.url.trim();
-    if (!options.url.startsWith('https://x.com') || !options.url.startsWith('https://twitter.com') || options.url.match(/\s/)) throw 'Not a valid Twitter link'
+    if (!options.url.startsWith('https://x.com') && !options.url.startsWith('https://twitter.com') && !options.url.match(/\s/)) throw 'Not a valid Twitter link'
     const url = options.url.replace('x.com', 'vxtwitter.com')
     return new MessageBuilder().content(url)
 })
